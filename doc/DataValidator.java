57a45bb3cd20199f315a55baeca94f7804431b58
Patrick Nyboe Kjøller <Patrick@ctrl.dk>
2023-11-20 09:47:59 +0100

    ghghjhjh

--- doc/DataValidator.java
+++ doc/DataValidator.java
@@ -2,5 +2,22 @@ package doc;
 
 public class DataValidator {
 
+    public static boolean isUserNameValid(String username) {
+        if(username == null || username.length() < 4 || username.length() > 50 ||  username.equals("")) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public static boolean isPasswordValid(String password) {
+        if(password == null || password.length() < 4 || password.length() > 50 ||  password.equals("")) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 
 }
